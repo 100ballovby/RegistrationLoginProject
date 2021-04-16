1e8aca90cf72acb6a9715883d152c3f0aa5ab462
demidraksin <greatraksin@icloud.com>
2021-04-16 14:25:10 +0300

    Adding registration form✅

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, request, url_for
 from forms import RegisterForm, LoginForm
+from models import User
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -14,7 +15,22 @@ def login_page():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register_page():
-    return render_template('register.html')
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate():
+        hashed_password = generate_password_hash(
+            form.password.data,
+            method='sha256')
+        new_user = User(
+            u=form.username.data,
+            p=hashed_password
+        )
+        db.session.add(new_user)  # сохранить юзера в бд
+        db.session.commit()  # применить изменения
+        # если регистрация успешна, вернуться на главную страницу
+        return redirect(url_for('main_page'))
+    else:
+        # показывать форму, когда заходят на страницу регистрации
+        return render_template('register.html', form=form)
 
